65586e40af735b246aeb46ff3995755bb8bfd560
Yuriy Shandor <yuriyshandor@gmail.com>
2017-12-25 16:11:15 +0200

    Add Improve Mod_02

--- js/script.js
+++ js/script.js
@@ -72,4 +72,17 @@ $(document).ready(function() {
     $('.pricing').removeClass('pricing-hover');
     $(this).addClass('pricing-hover');
   });
+
+  $('a[href^="#"]').on('click',function (e) {
+	  e.preventDefault();
+
+	  var target = this.hash;
+	  var $target = $(target);
+
+	  $('html, body').stop().animate({
+	     'scrollTop': $target.offset().top
+	    }, 700, 'swing', function () {
+	      window.location.hash = target;
+	  });
+	});
 });
